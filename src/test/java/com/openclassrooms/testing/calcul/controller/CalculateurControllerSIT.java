745354c532e66955a84f6e45bf3229310b08645a
delaval <delaval.htps@gmail.com>
2021-01-22 20:24:20 +0100

    ATDD & BDD configuré et terminé

---
+++ src/test/java/com/openclassrooms/testing/calcul/controller/CalculateurControllerSIT.java
@@ -0,0 +1,54 @@
+package com.openclassrooms.testing.calcul.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import javax.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import com.openclassrooms.testing.calcul.domain.Calculator;
+import com.openclassrooms.testing.calcul.service.CalculatorService;
+import com.openclassrooms.testing.calcul.service.SolutionFormatter;
+
+@WebMvcTest(controllers = {CalculatorController.class, CalculatorService.class})
+@ExtendWith(SpringExtension.class)
+public class CalculateurControllerSIT {
+
+  @Inject
+  private MockMvc mockMvc;
+
+  @MockBean
+  private SolutionFormatter solutionFormatter;
+
+  @MockBean
+  private Calculator calculator;
+
+  @Test
+  public void givenACalculatorApp_whenRequestToAdd_thenSolutionisShow() throws Exception {
+    // GIVEN
+    when(calculator.add(3, 2)).thenReturn(5);
+    when(solutionFormatter.format(5)).thenReturn("5");
+
+
+    // WHEN
+    MvcResult result = mockMvc
+        .perform(MockMvcRequestBuilders.post("/calculator").param("leftArgument", "3")
+            .param("calculationType", "ADDITION").param("rightArgument", "2"))
+        .andExpect(MockMvcResultMatchers.status().is2xxSuccessful()).andReturn();
+
+    // THEN
+    verify(calculator, times(1)).add(3, 2);
+    verify(solutionFormatter, times(1)).format(5);
+    assertThat(result.getResponse().getContentAsString()).contains("id=\"solution\"")
+        .contains(">5</span");
+
+  }
+}
---
+++ src/test/resources/features/cucumber-addition.feature
@@ -0,0 +1,10 @@
+# language:  fr
+
+Fonctionnalité:  Additioner deux nombres 
+ 
+
+  Scénario: Additionner deux nombres positifss
+    Etant donné un élève utilise le Calculateur
+    Quand 2 et 5 sont additionnés
+    Alors on montre 7 à l'élève
+
